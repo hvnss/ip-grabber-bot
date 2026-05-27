bb2a66c89d9a243fde2b0d3c52e9d1711fa6deb1
hvnss <136252572+hvnss@users.noreply.github.com>
2026-05-27 22:47:01 +0700

    Add start.sh to run both web and bot

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-gunicorn app:app --bind 0.0.0.0:$PORT
+echo "Starting web server (gunicorn)..."
+gunicorn app:app --bind 0.0.0.0:$PORT &
+
+echo "Starting Telegram bot..."
+python bot.py
